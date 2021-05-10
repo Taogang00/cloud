88e8f821b4bf23effd211dc544419e89e397fbe7
taogang <1102014862@qq.com>
2021-05-10 10:45:43 +0800

    demo

--- guanwei-cloud-account-server/src/main/java/com/guanwei/account/controller/AccountController.java
+++ guanwei-cloud-account-server/src/main/java/com/guanwei/account/controller/AccountController.java
@@ -2,36 +2,25 @@ package com.guanwei.account.controller;
 
 import com.guanwei.account.client.ResultData;
 import com.guanwei.account.client.ThirdFeignClient;
-import com.guanwei.order.client.OrderFeignClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping
 public class AccountController {
 
-    private final OrderFeignClient feignClient;
-
     private final ThirdFeignClient thirdFeignClient;
 
-    public AccountController(OrderFeignClient feignClient, ThirdFeignClient thirdFeignClient) {
-        this.feignClient = feignClient;
-        this.thirdFeignClient = thirdFeignClient;
-    }
-
     @GetMapping("/1")
-    public String getMsg() {
-        return feignClient.get("11").toString();
-    }
-
-    @GetMapping("/2")
-    public ResultData getMsg2() {
+    public ResultData getMsg1() {
         return thirdFeignClient.getSentences();
     }
 
-    @GetMapping("/3")
-    public  ResultData getMsg3() {
+    @GetMapping("/2")
+    public  ResultData getMsg2() {
         return thirdFeignClient.getPoetryFull(2, 1);
     }
 
